095ded7e41eded5fbeddc93dfc1ec8712ef32f99
Chloe Prime <chloe_von_einzbern@qq.com>
2024-11-30 06:20:08 +0800

    清理imports（此commit为真正的1.3.1）

--- src/main/java/mod/chloeprime/gunsmithlib/mixin/MixinLivingEntity.java
+++ src/main/java/mod/chloeprime/gunsmithlib/mixin/MixinLivingEntity.java
@@ -7,17 +7,12 @@ import mod.chloeprime.gunsmithlib.common.util.HurtFunction1;
 import mod.chloeprime.gunsmithlib.common.util.HurtFunction2;
 import mod.chloeprime.gunsmithlib.common.util.SpecialHurtable;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.function.DoubleConsumer;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity implements SpecialHurtable {
